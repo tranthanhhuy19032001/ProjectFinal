e00f1befc74c783d4284971c53e07856a230fdb9
Huy <tranthanhhuy19032001.ltp@gmail.com>
2022-12-05 02:16:56 +0700

    Update at 2h16 5/12

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,53 +1,57 @@
-const {mongooseToObject} = require('../../until/mongoose');
-const {multipleMongooseToObject} = require('../../until/mongoose');
-const User = require('../models/UserModel');
-const Home = require('../models/HomeModel');
-const multer = require('multer');
-const sharp = require('sharp');
+const { mongooseToObject } = require("../../until/mongoose");
+const { multipleMongooseToObject } = require("../../until/mongoose");
+const User = require("../models/UserModel");
+const Home = require("../models/HomeModel");
+const multer = require("multer");
+const sharp = require("sharp");
 
 const multerStorage = multer.memoryStorage();
 
 //Filter image (chua toi uu)
 const multerFilter = (req, file, cb) => {
-  // console.log(file.mimetype);
-  if (file.mimetype === 'image/jpeg') {
-    cb(null, true);
-  } else {
-    cb(new Error('File is not image', 400), false);
-  }
+    // console.log(file.mimetype);
+    if (file.mimetype === "image/jpeg") {
+        cb(null, true);
+    } else {
+        cb(new Error("File is not image", 400), false);
+    }
 };
 
 //Upload file image
 const upload = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter,
+    storage: multerStorage,
+    fileFilter: multerFilter,
 });
 
 class UserController {
-    
     info(req, res, next) {
+        console.log("2222222");
         const currentUser = res.locals.user;
-        res.render('user/thongtinnguoidung', {
-            currentUser: mongooseToObject(currentUser)
+        console.log("1111111");
+        console.log(currentUser.id);
+        res.render("user/thongtinnguoidung", {
+            currentUser: mongooseToObject(currentUser),
         });
     }
 
     stored(req, res, next) {
-        console.log(res.locals.user);
+        // console.log(res.locals.user);
         // collection.countDocuments({ "_id": { "$exists": true } });
-        
+
         let currentUser = res.locals.user;
         // const count = await Home.countDocuments({user: currentUser._id})
         // console.log(count);
-        
-      Promise.all([Home.find({ user: currentUser._id }), Home.countDocuments({user: currentUser._id})])
-          .then(([homes, count]) => 
-              res.render('user/newfeed', {
-                  count,
-                  homes: multipleMongooseToObject(homes),
-                  currentUser: mongooseToObject(currentUser)
-              })
-          )
+
+        Promise.all([
+            Home.find({ user: currentUser._id }),
+            Home.countDocuments({ user: currentUser._id }),
+        ]).then(([homes, count]) =>
+            res.render("user/newfeed", {
+                count,
+                homes: multipleMongooseToObject(homes),
+                currentUser: mongooseToObject(currentUser),
+            })
+        );
         // Home.find({ user: currentUser._id })
         //     .then(homes =>{
         //         res.render('user/newfeed', {
@@ -55,7 +59,6 @@ class UserController {
         //             homes: multipleMongooseToObject(homes)
         //         });
         // })
-
 
         // let newsId = newsOfUser.map((el) => el._id);
         // console.log(newsId)
@@ -68,45 +71,70 @@ class UserController {
         //     console.log(el)
         //     currentUser.home = newsId.map((el) => (el));
         // });
-        
 
-        
         // res.json(newsId)
-        
     }
     edit(req, res, next) {
         let currentUser = res.locals.user;
         Home.findById(req.params.id)
-            .then(home => res.render('user/edit',{
-                home: mongooseToObject(home),
-                currentUser: mongooseToObject(currentUser),
-            }))
+            .then((home) =>
+                res.render("user/edit", {
+                    home: mongooseToObject(home),
+                    currentUser: mongooseToObject(currentUser),
+                })
+            )
             .catch(next);
     }
 
     update(req, res, next) {
-        upload.single('upFile')(req, res, (next) => {
+        upload.single("upFile")(req, res, (next) => {
             // console.log(req.file);
             sharp(req.file.buffer)
-              .resize(214, 148)
-              .toFormat('jpeg')
-              .jpeg({ quality: 90 })
-              .toFile(`./src/public/img/${req.file.originalname}`);
-      
+                .resize(214, 148)
+                .toFormat("jpeg")
+                .jpeg({ quality: 90 })
+                .toFile(`./src/public/img/${req.file.originalname}`);
+
             let currentUser = res.locals.user;
             let formData = req.body;
             formData.images = req.file.originalname;
             formData.user = currentUser._id;
-            Home.updateOne({_id: req.params.id}, formData)
-            .then(() => res.redirect('/user/stored'))
-            .catch(next);
-      
+            Home.updateOne({ _id: req.params.id }, formData)
+                .then(() => res.redirect("/user/stored"))
+                .catch(next);
         });
-        
     }
-    
-    
+    async updatePassword(req, res, next) {
+        // res.json(req.body)
+        const user = await User.findById(res.locals.user);
+        // res.json(user)
+
+        //2) Check if POSTed currentPassword is correct
+        // if (!(await user.correctPassword(req.body.currentPassword, user.password))) {
+        //   return next(new Error("Your current password is wrong", 401));
+        // }
+        // if (req.body.NewPasswordUser != req.body.ConfirmNewPasswordUser) {
+        //   return next(
+        //     new Error(
+        //       "Your new password is not equal confirm password! Please enter again password!",
+        //       401
+        //     )
+        //   );
+        // }
+        // console.log('1');
+        //3) if so, update Password
+        user.password = req.body.NewPasswordUser;
+        user.passwordConfirm = req.body.ConfirmNewPasswordUser;
+
+        // console.log(user.password);
+        // console.log(user.passwordConfirm);
+
+        user.save().then(() => {
+            res.redirect("back");
+        });
+
+        //4) Login user, sent JWT
+    }
 }
 
 module.exports = new UserController();
-
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -9,6 +9,7 @@ router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 router.get('/stored', authController.protect, authController.isLoggedIn, userController.stored);
 router.put('/stored/:id', authController.protect, authController.isLoggedIn, userController.update);
+router.put('/:id/change', authController.protect, authController.isLoggedIn, userController.updatePassword);
 router.get('/:id/edit', authController.protect, authController.isLoggedIn, userController.edit);
 
 
